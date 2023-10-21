cd797f01667dbc2f3c1219801fd2bb53ec989b64
401122005studentnumber <146081831+401122005studentnumber@users.noreply.github.com>
2023-10-21 11:16:09 -0700

    Account.h

---
+++ Account.h
@@ -0,0 +1,14 @@
+#include <string>
+
+class Account{
+    public:
+         std::string getName() const{;
+         name="Abass";
+         return name;
+}
+    void setName(std::string theName){
+        name=theName;
+    }
+    private:
+    std::string name:
+}
